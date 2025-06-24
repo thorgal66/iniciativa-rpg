382362f7cc1a7281d389ed5f03a3a0f8bbea993f
thorgal66 <batsegal@proton.me>
2025-06-24 03:34:43 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,19 @@
-const CACHE_NAME = "iniciativa-rpg-cache-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("iniciativa-rpg").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./service-worker.js",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png"
+      ]);
+    })
   );
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
